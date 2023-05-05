cce727418a58691e476051e0b2b255113065c64d
masamarojevic <marojevicmasa@gmail.com>
2023-05-05 12:01:52 +0200

    removed db

--- QuireApp/app/src/main/java/com/example/quireapp/LoggFragment.kt
+++ QuireApp/app/src/main/java/com/example/quireapp/LoggFragment.kt
@@ -6,13 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.quireapp.databinding.FragmentLoggBinding
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+
 
 class LoggFragment : Fragment() {
     private var _binding:FragmentLoggBinding?=null
     private val binding get() = _binding!!
-    private lateinit var db : DatabaseReference
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +27,6 @@ class LoggFragment : Fragment() {
         _binding = FragmentLoggBinding.inflate(layoutInflater,container,false)
         val view = binding.root
 
-        db = FirebaseDatabase.getInstance("https://quire-9ac5c-default-rtdb.europe-west1.firebasedatabase.app")
-            .getReference("User")
         return view
     }
 
